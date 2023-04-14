287475cba3ae9e419b3693bffe41a9ee0cfd86b3
Italo Araujo <italojsouza@gmail.com>
2023-04-14 17:31:25 -0300

    Finalizada paleta aleatoria e salva

--- script.js
+++ script.js
@@ -1,23 +1,48 @@
 let numColors = 3;
 
-const colorPallet = document.getElementById('color-palette');
+const colorPallete = document.getElementById('color-palette');
 let colors = [];
-
-const nameColors = [
-    'red',
-    'green',
-    'blue'
-];
 
 const randomRGB = () => {
  return `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
 }
 
-for (let index = 0; index < numColors; index += 1) {
-    colors[index] = document.createElement('div');
-    colors[index].classList.add('color', 'circle');
-    colors[index].style.backgroundColor = randomRGB();
-    colorPallet.appendChild(colors[index]);
+if (localStorage.coresPuras) {
+    let coresPurasRgb = JSON.parse(localStorage.coresPuras);
+    for (let index = 0; index < numColors; index += 1) {
+        colors[index] = document.createElement('div');
+        colors[index].classList.add('color', 'circle');
+        colors[index].style.backgroundColor = coresPurasRgb[index];
+        colorPallete.appendChild(colors[index]);
+    }
+} else {
+    for (let index = 0; index < numColors; index += 1) {
+        colors[index] = document.createElement('div');
+        colors[index].classList.add('color', 'circle');
+        colors[index].style.backgroundColor = randomRGB();
+        colorPallete.appendChild(colors[index]);
+    }
 }
+
+
+const createPallette = () => {
+    for (let index = 0; index < numColors; index += 1) {
+        colors[index].style.backgroundColor = randomRGB();
+    }
+}
+
+
+const savePallette = () => { 
+    let coresPurasRgb = [];  
+    for (let index = 0; index < numColors; index += 1) {
+        coresPurasRgb[index] = colors[index].style.backgroundColor;
+    }
+    localStorage.setItem('coresPuras', JSON.stringify(coresPurasRgb));
+}
+
+
+const buttonPallete = document.getElementById('button-random-color');
+buttonPallete.addEventListener('click', createPallette);
+buttonPallete.addEventListener('click', savePallette);
 
 
